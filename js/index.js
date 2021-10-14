770953aca91c0bfc7d16147a3f5a05449b0bf81e
iamericsu <89247843+iamericsu@users.noreply.github.com>
2021-10-14 15:07:46 +0800

    20211014_update_1
    依助教回饋調整相關內容:
    * head 增加描述
    * 移除不用設定 flex 的部分
    * 設定的樣式相同，合併為一個 class

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 for(let i = 1; i <= 3; i++){
-    let expDot = document.querySelector(".exp-timeline-dot-" + i);
-    let expBox = document.querySelector(".exp-timeline-box-" + i);
+    let expDot = document.querySelector(".exp-time-dot-" + i);
+    let expBox = document.querySelector(".exp-time-box-" + i);
     expDot.addEventListener('mouseover', function(){
         expBox.classList.toggle("mouseEvent");
     });
